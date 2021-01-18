00fb95b0bf76009ab0a4cee1e020954213e3736f
Kattkovacs <kattkovacs@gmail.com>
2021-01-18 18:24:18 +0100

    add PokemonRepository to repository

---
+++ pokemon/src/main/java/com/codecool/pokemon/repository/PokemonRepository.java
@@ -0,0 +1,14 @@
+package com.codecool.pokemon.repository;
+
+import com.codecool.pokemon.model.Pokemon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PokemonRepository extends JpaRepository<Pokemon, Long> {
+
+    @Override
+    Optional<Pokemon> findById(Long aLong);
+}
