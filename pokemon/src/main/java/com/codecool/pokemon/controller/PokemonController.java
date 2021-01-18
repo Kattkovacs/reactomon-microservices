5a11a12140768dedc45d19fcc4845d11717f6736
Kattkovacs <kattkovacs@gmail.com>
2021-01-18 20:22:48 +0100

    add PokemonController

---
+++ pokemon/src/main/java/com/codecool/pokemon/controller/PokemonController.java
@@ -0,0 +1,26 @@
+package com.codecool.pokemon.controller;
+
+import com.codecool.pokemon.model.Pokemon;
+import com.codecool.pokemon.repository.PokemonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pokemons")
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
+public class PokemonController {
+
+    @Autowired
+    PokemonRepository pokemonRepository;
+
+    @GetMapping("/all")
+    public List<Pokemon> getAllVideos(){
+        return pokemonRepository.findAll();
+    }
+
+}
