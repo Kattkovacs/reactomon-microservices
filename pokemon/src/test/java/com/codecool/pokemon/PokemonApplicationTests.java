44ac07babda871da9cf5f179a9ff1d3cf13d910e
Kattkovacs <kattkovacs@gmail.com>
2021-01-18 18:05:27 +0100

    Initial commit

---
+++ pokemon/src/test/java/com/codecool/pokemon/PokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
