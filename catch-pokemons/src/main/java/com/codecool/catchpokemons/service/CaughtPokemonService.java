4b591bb4ba5d607cf37c34dfb59d1f2e7a80074b
derdesz <dora.mailbox@gmail.com>
2021-01-19 15:49:28 +0100

    add java List import

--- catch-pokemons/src/main/java/com/codecool/catchpokemons/service/CaughtPokemonService.java
+++ catch-pokemons/src/main/java/com/codecool/catchpokemons/service/CaughtPokemonService.java
@@ -5,6 +5,8 @@ import com.codecool.catchpokemons.repository.CaughtPokemonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CaughtPokemonService {
 
